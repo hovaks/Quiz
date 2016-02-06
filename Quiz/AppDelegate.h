7bf0e76ffcc92706f32a06930ada275a1aade44b
hovaks <hovaks@gmail.com>
2016-02-06 20:50:55 +0400

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Hovak Davtyan on 06.02.16.
+//  Copyright © 2016 Hovak Davtyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Hovak Davtyan on 06.02.16.
+//  Copyright © 2016 Hovak Davtyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
